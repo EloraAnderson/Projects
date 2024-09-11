a99ce69111fcc5859d5a7b97e3c3e87b3a491736
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-11 01:37:15 +0000

    5th line problems

--- test folder/test.c
+++ test folder/test.c
@@ -4,7 +4,7 @@ int main(void){
    char name[] = "Elora";
    printf("%s", name);
    char anum[] = "7";
-   printf("%s", anum);
+   printf("%s", anum):
    char anum2[] = "293";
    printf("%s", anum2);
    char breakfast[] = "nothing";
