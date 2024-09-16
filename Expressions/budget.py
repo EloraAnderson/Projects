b9fd19af9660dc03d2cb2243929639377ac21490
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-16 20:29:35 +0000

    no spaces

--- Expressions/budget.py
+++ Expressions/budget.py
@@ -1,23 +1,18 @@
 print("This is going to calculate your budget for the month.")
-
 income = float(input("How much do you make a month? $"))
 print(f"Your income is: ${income:.2f}")
-
 rent = float(input("How much is rent? $"))
 utilities = float(input("How much are your utilities? $"))
 groceries = float(input("How much are groceries? $"))
 transportation = float(input("How much is transportation? $"))
-
 expenses = rent + utilities + groceries + transportation
 savings = income * 0.2
 total = income - expenses - savings
-
 prent = rent / income
 putilities = utilities / income
 pgroceries = groceries / income
 ptransportation = transportation / income
 pexpenses = expenses / income
-
 print(f"Your expenses are: ${expenses:.2f}")
 print(f"Your savings are: ${savings:.2f}")
 print(f"Your total left to spend is: ${total:.2f}")
