64cbd108936414e15121ee9eb62719058731fd84
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-16 20:35:38 +0000

    math

--- Expressions/expression.c
+++ Expressions/expression.c
@@ -1,5 +1,38 @@
 #include <math.h>
 
+int main(void){
+   int numOne, numTwo, numTree, numFour, numFiv, numSix, numSeven, numEigt, numNine, numTen, numEleven, numTwelve;
+   int numThrt, numFrt, numFif, numSixt, numSevt, numEigten, numNint, numTwny, numTyone;
+   printf("%d \n", 7-24/8*4+6);
+   printf("%d \n", 18/3-7+2*5);
+   printf("%d \n", 6*4/12+72/8-9);
+   numOne = 17-6/2;
+   printf("%d \n", numOne+4*3);
+   numTwo = -2*1;
+   numTree = -2*4;
+   numFour = -2*2;
+   numFiv = numTwo*numTree-numFour/numFour;
+   numSix = 6+2-3;
+   printf("%d \n", numFiv+numSix);
+   numSeven = 3-4*7;
+   numEigt = numSeven/5;
+   printf("%d \n", -1*numEigt-2*24/6);
+   numNine = pow(5,2);
+   numTen = 3*numNine/15;
+   numEleven = pow(2,2);
+   numTwelve = 5-numEleven;
+   printf("%d \n", numTen-numTwelve);
+   numThrt = pow(1,4);
+   numFrt = pow(2,2);
+   numFif = pow(3,3);
+   numSixt = numThrt*numFrt+numFif;
+   numSevt = pow(2,5);
+   printf("%d \n", numSixt-numSevt/4);
+   numEigten = 22/2-2*5;
+   numNint = pow(numEigten,2);
+   numTwny = 4-6/6;
+   numTyone = pow(numTwny,2);
+   printf("%d \n", numNint+numTyone);
+   return 0;
+}
 
-int num =(int)pow(2,5);
-printf("%f", num);
