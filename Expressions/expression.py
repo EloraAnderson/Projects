d3499d4f6fe8142cc2e7d6207748ef834ae95b5b
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-17 01:14:30 +0000

    changed expression

--- Expressions/expression.py
+++ Expressions/expression.py
@@ -1,8 +1,27 @@
-one = 1 
-seven = 7
-twentyfour = 24
-eight = 8
-four = 4
-six = 6
- 
- one = seven - twentyfour / eight * four + six
+print(7-24/8*4+6)
+print(18/3-7+2*5)
+print(6*4/12+72/8-9)
+frstnum = 17-6/2
+print(frstnum+4*3)
+secondNum = -2*1
+thirdNum = -2*4
+frothNum = -2*2
+fifthNum = secondNum*thirdNum-frothNum/frothNum
+sixNum = 6+2-3
+print(fifthNum+sixNum)
+sevenNum = 3-4*7
+ateNum = sevenNum/5
+nineNum = -1*ateNum
+print(nineNum-2*24/6)
+tenNum = 3*5**2/15
+elevenNum = 5-2**2
+print(tenNum-elevenNum)
+twelveNum = 1**4*2**2+3**3
+thrtNum = 2**5/4
+print(twelveNum-thrtNum)
+fortenNum = 22/2-2*5
+fiftenNum = fortenNum**2
+sixtenNum = 4-6/6
+sevtenNum = sixtenNum**2
+print(fiftenNum+sevtenNum)
+
