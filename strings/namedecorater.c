0751fc40777d989345a5a7bf32dc4eabb7daa37f
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-20 03:12:04 +0000

    attemptingwithc

---
+++ strings/namedecorater.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[30];
+    printf("please tell me your name:  );
+    scanf("%s", "<(*0*)>" , name, "<(*0*)>");
+    printf("Hello %s", name);
+
+return 0;
+}
