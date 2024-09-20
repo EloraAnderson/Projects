514f5a6ebf99aacc57253817db40a44d31961bf7
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-20 20:13:41 +0000

    silly names py

---
+++ strings/sillysentences.py
@@ -0,0 +1,5 @@
+name = input("enter a name")
+namedos = input("enter a second name")
+place = input("enter a place")
+verb = input("enter a verb")
+print(name + " and " + namedos + " went to " + place + " so they could " + verb + ".")
