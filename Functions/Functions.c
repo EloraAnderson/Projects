8f18150f1275c65bead65d69b609b84a06e4f7ac
EloraAnderson <lou.anderson@ucas-edu.net>
2024-09-26 20:34:11 +0000

    finnnished with functions updte

--- Functions/Functions.c
+++ Functions/Functions.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+void Hello(char name[]){
+    printf("Hello %s \n", name);
+}
+
+int main(void){
+    Hello("Maddie");
+    Hello("Cephas");
+    Hello("William");
+    Hello("Val");
+    Hello("Cadence");
 
     
+    return 0;
 
-return 0;
 }
