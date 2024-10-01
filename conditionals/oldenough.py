59aaf95f5b178ddcc8cde2a4eb058ac33671635f
EloraAnderson <lou.anderson@ucas-edu.net>
2024-10-01 20:12:38 +0000

    doneish

---
+++ conditionals/oldenough.py
@@ -0,0 +1,12 @@
+age = 2
+if age >=18:
+    print("you are able to vote")
+elif age >=16:
+    print("you are able to drive")
+elif age >=15:
+    print("you are able to get a learners permit")
+elif age >=5:
+    print("you are old enough to go to elementry school")
+else:
+    print("you are too young to go to school")
+
