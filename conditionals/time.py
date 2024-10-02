10097fc26445071586ca0dd0faa9c0906c5270b7
EloraAnderson <lou.anderson@ucas-edu.net>
2024-10-02 20:38:01 +0000

    saved gtime c

--- conditionals/time.py
+++ conditionals/time.py
@@ -3,3 +3,12 @@ import datetime
 now = datetime.datetime.now().hour
 
 print(now)
+
+if now <= 12:
+    print("good morning")
+elif now <= 18:
+    print("good afternoon")
+elif now <= 20:
+    print("good evening")
+else:
+    print("good night")
