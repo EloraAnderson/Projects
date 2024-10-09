43911c01a712f2a9c03b251276feab33a71f2e55
EloraAnderson <lou.anderson@ucas-edu.net>
2024-10-09 20:28:19 +0000

    glitter c

--- strings/namedecorater.c
+++ strings/namedecorater.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    char name[30];
+    char glitter[30],name[50];
     printf("please tell me your name:  ");
     scanf("%s", name );
-    printf("Hello <(*0*)>%s<(*0*)>", name);
+    strcat(glitter, "<(*~*)>");
+    strcat(glitter, name);
+    strcat(glitter, "<(*~*)>");
+    strcat(glitter, "\n");
+    printf("%s", glitter);
 
 return 0;
 }
