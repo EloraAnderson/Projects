ea9178152d16ee25730a01a66f7345a84de1e47e
EloraAnderson <lou.anderson@ucas-edu.net>
2024-10-09 20:13:03 +0000

    list family c

--- lists/lists.c
+++ lists/lists.c
@@ -2,8 +2,8 @@
 char siblings[8][20] = {"Ember", "Ashlyn", "Elora", "Cole"};
 int i;
 int main (){
-    for(i=0; i < 8; i++){
-        printf("%s\n", siblings[i]);
+    for(i=0; i < 4; i++){
+        printf("Hello %s\n", siblings[i]);
     }
     return 0;
 }
