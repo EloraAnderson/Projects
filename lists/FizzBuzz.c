72c7a2f99f341d4b50606d61d9a8df172333c8b1
EloraAnderson <lou.anderson@ucas-edu.net>
2024-10-11 20:07:26 +0000

    fizzbuzz done c

--- lists/FizzBuzz.c
+++ lists/FizzBuzz.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
-int i;
-int usr;
+#include <string.h>
+#include <time.h>
+int i, usr;
+char one[50],two[50],three[50];
+
+void delay(int sec){
+    int milli = 1000*sec;
+
+    clock_t start = clock();
+
+    while(clock()< start+milli);
+}
+
+
 int main(){
     printf("Write the number you want to use: \n");
-    scanf("%d", usr);
+    scanf("%d", &usr);
+    printf("Give me a word: \n");
+    scanf("%s", one);
+    printf("Give me another word: \n");
+    scanf("%s", two);
+    strcat(three, one);
+    strcat(three, two);
     //create a loop that count to 50
-    for(i=1;i<51;i++){
+    for(i=1;i<usr;i++){
         //and replace numbers divisible by 5 and 3 with "fizzbuzz"   
         if(i%3==0 && i%5==0){
-            printf("FizzBuzz\n");
+            printf("%s\n", three);
         //replace numbers dicisble by 3 with "fizz"
         }else if(i%3==0){
-            printf("Fizz\n");
+            printf("%s\n", one);
         //replace numbers divisible by 5 with "buzz"
         }else if (i%5==0){
-            printf("Buzz\n");
+            printf("%s\n", two);
          //those that dont met the criteria will be regular printed numbers
         }else{
             printf("%d\n", i);
         }
+        delay(50);
     }
     return 0;
 }
